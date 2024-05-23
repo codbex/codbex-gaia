b2503ada2c29be4a1970f0d951e445394d170084
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-23 12:29:36 +0300

    Apply codbex formatter

--- application/src/main/java/com/codbex/gaia/GaiaApplication.java
+++ application/src/main/java/com/codbex/gaia/GaiaApplication.java
@@ -1,9 +1,8 @@
 /*
  * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -22,7 +21,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = {"com.codbex.gaia", "org.eclipse.dirigible.components"})
 @EnableScheduling
 public class GaiaApplication {
-	
+
     public static void main(String[] args) {
         SpringApplication.run(GaiaApplication.class, args);
     }
