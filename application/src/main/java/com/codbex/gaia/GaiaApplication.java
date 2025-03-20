27b29ba56b79a0dffb7bff2a921634bec01d8c5e
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-20 14:25:04 +0200

    fix-terminal

--- application/src/main/java/com/codbex/gaia/GaiaApplication.java
+++ application/src/main/java/com/codbex/gaia/GaiaApplication.java
@@ -22,7 +22,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class GaiaApplication {
 
+    private static long startedAt;
+
+    public static long getStartedAt() {
+        return startedAt;
+    }
+
     public static void main(String[] args) {
+        startedAt = System.currentTimeMillis();
+        System.out.println("------- Application is starting -------");
         SpringApplication.run(GaiaApplication.class, args);
     }
 
