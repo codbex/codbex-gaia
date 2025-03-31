342ae22bfccd95ebe82f7eb54a0db4394d384234
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:13:49 +0300

    delete not needed tests

--- integration-tests/src/test/java/com/codbex/gaia/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/gaia/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -2,18 +2,12 @@ package com.codbex.gaia.integration.tests;
 
 import org.eclipse.dirigible.integration.tests.api.SecurityIT;
 import org.eclipse.dirigible.integration.tests.api.java.messaging.MessagingFacadeIT;
-import org.eclipse.dirigible.integration.tests.api.javascript.cms.CmsSuiteIT;
-import org.eclipse.dirigible.integration.tests.api.rest.ODataAPIIT;
-import org.eclipse.dirigible.integration.tests.ui.tests.MailIT;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({//
-        CmsSuiteIT.class, //
-        MailIT.class, //
         MessagingFacadeIT.class, //
-        ODataAPIIT.class, //
         SecurityIT.class, //
 })
 public class DirigibileCommonTestSuiteIT {
